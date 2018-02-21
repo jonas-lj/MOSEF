82a58647e47f6d3501acdeba16bbc4a9aefb607a
Jonas Lindstrøm <jonas.l.jensen@alexandra.dk>
2018-02-21 23:12:59 +0100

    Fix echo bug

--- src/dk/jonaslindstrom/mosef/modules/delay/Echo.java
+++ src/dk/jonaslindstrom/mosef/modules/delay/Echo.java
@@ -25,7 +25,7 @@ public class Echo extends CompositeModule {
 		Module[] times = m.split(time, echos);
 		Module[] delays = new Module[echos];
 		for (int i = 1; i <= echos; i++) {
-			Module delay = m.amplifier(m.delay(split[i], m.amplifier(times[i], i), max * i), 0.2f);
+			Module delay = m.amplifier(m.delay(split[i], m.amplifier(times[i - 1], i), max * i), 0.2f);
 			delays[i - 1] = delay;
 		}
 		return m.mixer(split[0], m.mixer(delays));
